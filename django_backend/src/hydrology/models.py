d305036ba5ba11c439d5000da3efa0198b1feed0
yes <yes@yes.note>
2020-03-09 02:25:36 +0600

    Added Hydrostation category

--- django_backend/src/hydrology/models.py
+++ django_backend/src/hydrology/models.py
@@ -3,9 +3,18 @@ from django.contrib.gis.db import models
 from common.models import Station
 
 
-class Hydrostation(Station):
-
+class HydrostationCategory(models.Model):
+    code = models.CharField(primary_key=True, max_length=15)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
+
+class Hydrostation(Station):
+    category = models.ForeignKey(HydrostationCategory, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
 
