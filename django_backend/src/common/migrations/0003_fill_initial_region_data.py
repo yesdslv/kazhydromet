ccda8f4c9fca0a6c609c26d13c3b027ac6957216
yes <yes@yes.note>
2020-03-09 02:16:36 +0600

    Initial data for region model

---
+++ django_backend/src/common/migrations/0003_fill_initial_region_data.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models
+
+from common.initial_data import regions
+
+
+def fill_region_initial_data(apps, schema_editor):
+    Region = apps.get_model('common', 'Region')
+    for region in regions:
+        Region.objects.create(code=region['pk'], name=region['name'])
+
+
+def delete_region_initial_data(apps, schema_editor):
+    Region = apps.get_model('common', 'Region')
+    Region.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0002_region'),
+    ]
+
+    operations = [
+        migrations.RunPython(fill_region_initial_data, delete_region_initial_data)
+    ]
